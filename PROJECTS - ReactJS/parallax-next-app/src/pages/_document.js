3fb2c9af41349f79389d4e6324506a3893dd5689
Omar Khan <33379302+OmarKhanGithub@users.noreply.github.com>
2024-04-21 01:40:23 -0300

    Parallax Project starter code - 1

---
+++ PROJECTS - ReactJS/parallax-next-app/src/pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+	return (
+		<Html lang="en">
+			<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;400;600;800&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
---
+++ PROJECTS - ReactJS/parallax-next-app/src/pages/index.js
@@ -0,0 +1,22 @@
+import Parallax from '@/components/Parallax';
+
+function Home() {
+	return (
+		<>
+			<Parallax />
+			<div className="about">
+				<h2>Lorem ipsum</h2>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit laborum
+					ipsam corrupti asperiores magnam quos cumque animi tempore vero
+					repellendus, harum odio neque quis, non temporibus. Inventore
+					asperiores repudiandae praesentium ut, fugit quo esse, placeat ullam
+					quibusdam perspiciatis delectus ducimus nihil. Dolorum nam veniam
+					aperiam sapiente corporis! Quisquam, veritatis repellendus?
+				</p>
+			</div>
+		</>
+	);
+}
+
+export default Home;
